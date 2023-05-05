587ca988e441f337155c0676c8e7a41332391c30
duythai12A <duythai10a@gmail.com>
2023-05-05 11:28:59 +0700

    add cart

--- src/main/java/com/example/webbanquanao/service/ShoppingCartService.java
+++ src/main/java/com/example/webbanquanao/service/ShoppingCartService.java
@@ -1,4 +1,22 @@
 package com.example.webbanquanao.service;
 
+import com.example.webbanquanao.entity.Cart;
+
+import java.util.Collection;
+
 public interface ShoppingCartService {
+    void add(Cart item);
+
+
+    void remove(Long id);
+
+    Cart update(Long proId, Long quanTiTy);
+
+    void clear();
+
+    Collection<Cart> getAll();
+
+    int getCount();
+
+    double getAmount();
 }
